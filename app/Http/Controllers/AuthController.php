8432fd4b1f4d3e63ffa00417eca0f03c74aee71c
CHANG180-PC\chang <chang180@gmail.com>
2024-05-05 11:53:02 +0800

    Episode 2 finished

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApiLoginRequest;
 use App\Traits\ApiReponses;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     use ApiReponses;
-    public function login(){
-        return $this->ok('Hello Trait!');
+    public function login(ApiLoginRequest $request){
+        return $this->ok($request->get('email'));
+    }
+
+    public function register(){
+        return $this->ok('register');
     }
 }
