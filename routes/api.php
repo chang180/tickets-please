997a2d6a04cbffc182a2d7d0c342fa3c92931a9f
張建文 <chang180@gmail.com>
2024-05-06 10:25:31 +0800

    Episode 3 finished.

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,22 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// http://ticketing.test/api
+// universal resource locator
+// tickets
+// users
+
+
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/tickets', function () {
+    return Ticket::all();
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
